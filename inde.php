5695eaebd0a7e0e6bd9a515564e4ce4cfd1bd242
Lejon <52333278+LeonLav77@users.noreply.github.com>
2020-11-07 19:08:37 +0100

    Update inde.php

--- inde.php
+++ inde.php
@@ -6,21 +6,24 @@
     <link rel="stylesheet" href="main.css">
   </head>
   <body>
-     <img src="pajo_papirnica_logo.gif" alt="pajo_papirnica_logo" height="100px" >
-      <br>
-      <br>
+    <img class="img1" src="pajo_papirnica_logo.gif" alt="pajo_papirnica_logo" height="100px">
       <center>
     <ul>
-<<<<<<< HEAD:inde.html
-      <li><a href="#top">Početna</a></li>
-=======
-      <li><a href="#">kucica</a></li>
->>>>>>> 0e9eb4b8d4b9a2b46abc455d65e75c038474e48e:inde.php
-      <li><a href="#">O Nama</a></li>
-      <li><a href="#">Asortiman</a></li>
-      <li><a href="#">Kontakt</a></li>
+      <li class="pocetna"><a href="#">Početna</a></li>
+      <li class="O_nama"><a href="#">O Nama</a></li>
+      <li class="asortiman"><a href="#">Asortiman</a></li>
+      <li class="kontakt"><a href="#">Kontakt</a></li>
     </ul>
-      </center>
+  </center>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img src="logo_2.png" alt="">
+  </center>
   </body>
 </html>
-<!--promjenio sam u .php da ja  moren radit u njemu osim tega nis nisan minja zasad tebi bbi tribalo sve ostat isto,jedinjo ako nemozes otvorit file onda ga preimenuj natrag u .html-->
