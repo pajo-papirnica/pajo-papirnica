7600703e03059caea8c88fa79b43502fe4f7f40e
MaskoMasko <persic.massimo@gmail.com>
2020-11-08 15:42:58 +0100

    Create inde.php

---
+++ inde.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PAPIRNICA PAJO</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <img class="img1" src="pajo_papirnica_logo.gif" alt="pajo_papirnica_logo" height="100px">
+      <center>
+    <ul>
+      <li class="pocetna"><a href="#">Početna</a></li>
+      <li class="O_nama"><a href="#">O Nama</a></li>
+      <li class="asortiman"><a href="#">Asortiman</a></li>
+      <li class="kontakt"><a href="#">Kontakt</a></li>
+    </ul>
+  </center>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <img src="logo_2.png" alt="">
+  </center>
+  </body>
+</html>
